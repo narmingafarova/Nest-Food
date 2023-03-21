85754c0bdc556d3b5f0aac4f7251839791737d2d
narmingafarova <qafarovanermin72@gmail.com>
2023-03-21 23:23:32 +0400

    addToCart quantity&price

--- src/components/addcart/AddTable.jsx
+++ src/components/addcart/AddTable.jsx
@@ -1,49 +1,133 @@
-import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table';
-import { useCart } from 'react-use-cart';
+// import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { ChevronDown, ChevronUp } from "react-bootstrap-icons";
+import Table from "react-bootstrap/Table";
+import { useCart } from "react-use-cart";
 
 function AddTable() {
-    const { items, updateItemQuantity, removeItem, cartTotal, emptyCart } = useCart();
+  const { items, updateItemQuantity, removeItem, cartTotal, emptyCart } =
+    useCart();
+  // const [quant, setQuant] = useState("");
+//   const [price, setPrice] = useState("");
 
-    return (
-        <Container className='add-table mb-5'>
-            <Table className='w-75 my-0 mx-auto'>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Subtotal</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map((item, count) => (
-                        <tr>
-                            <td>
-                                <a href="/cart" className='text-decoration-none delete-btn'>x</a>
-                            </td>
-                            <td>
-                                <a href="/">
-                                    <img width={50} src={item.image.imgFront} alt={item.title} />
-                                </a>
-                            </td>
-                            <td>
-                                <a href="/cart" className='text-decoration-none'>{item.title}</a>
-                            </td>
-                            <td>${item.price.discount * item.quantity}</td>
-                            <td>
-                                <input type="number" value="1" min="0" />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-
-        </Container>
-    );
+  return (
+    <Container className="add-table mb-5">
+      <Table className="shop-cart-items w-75 my-0 mx-auto">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, count) => (
+            <tr key={item.id}>
+              <td>
+                <a
+                  href="/cart"
+                  className="text-decoration-none delete-btn"
+                  onClick={() => {
+                    removeItem(item.id);
+                  }}
+                >
+                  x
+                </a>
+              </td>
+              <td>
+                <a href="/">
+                  <img width={50} src={item.image.imgFront} alt={item.title} />
+                </a>
+              </td>
+              <td>
+                <a href="/cart" className="text-decoration-none">
+                  {item.title}
+                </a>
+              </td>
+              <td>${item.price.discount}</td>
+              <td className="position-relative">
+                <input
+                  id="number"
+                  type="number"
+                  className="quantity text-center"
+                  defaultValue={item.quantity}
+                  min="0"
+                  step="1"
+                  value={item.quantity}
+                  onChange={(e) => (item.quantity = e.target.value)}
+                />
+                <div className="input-btns d-flex flex-column position-absolute">
+                  <Button
+                    className="py-0"
+                    onClick={() => {
+                      updateItemQuantity(item.id, item.quantity + 1);
+                    }}
+                  >
+                    <ChevronUp color="#3bb77e" fontSize={10} />
+                  </Button>
+                  <Button
+                    className="py-0"
+                    onClick={() => {
+                      updateItemQuantity(item.id, item.quantity - 1);
+                    }}
+                  >
+                    <ChevronDown color="#3bb77e" fontSize={10} />
+                  </Button>
+                </div>
+              </td>
+              <td>${(item.price.discount * item.quantity).toFixed(2)}</td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan={6}>
+              <div className="coupon-update d-flex justify-content-between">
+                <div className="coupon d-flex">
+                  <input type="text" placeholder="Coupon Code" />
+                  <Button
+                    type="submit"
+                    className="add-cart-btn ms-2 d-flex align-items-center"
+                  >
+                    Apply coupon
+                  </Button>
+                </div>
+                <div className="update">
+                  <Button className="add-cart-btn d-flex align-items-center">
+                    Update cart
+                  </Button>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <Row className="d-flex flex-row-reverse mt-4">
+        <Col sm={12} md={6}>
+          <h5>Cart totals</h5>
+          <Table bordered className="totals">
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>100</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>100</td>
+              </tr>
+            </tbody>
+          </Table>
+          <Button
+            type="submit"
+            className="add-cart-btn d-flex align-items-center"
+          >
+            Proceed to checkout
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default AddTable;
-
--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -2,51 +2,79 @@ import { useState } from 'react'
 import { Button, Col } from 'react-bootstrap'
 import { Cart, Star } from 'react-bootstrap-icons'
 import { useCart } from 'react-use-cart'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // import { ProductContext } from '../context/ProductContext'
 
 const ProductCard = ({ allData, frontImage, backImage, category, title, store, price, discount }) => {
-    const {addItem} = useCart();
-    // const [products] = useContext(ProductContext);
     const [img, setImg] = useState(frontImage);
+    const { addItem } = useCart();
+
+    const succesNotify = () =>
+        toast.success(`"${title}" added`, {
+            position: "bottom-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        })
+    // const [products] = useContext(ProductContext);
     // const [proPercent, setPropercent] = useState("");
     // const [perBgc, setPerbgc] = useState("");
 
     // style={{ backgroundColor: `${perBgc}` }} {proPercent}
     return (
-        <Col sm={6} md={2}>
-            <div className='product-card h-100 py-3' onMouseEnter={() => { setImg(backImage) }} onMouseLeave={() => { setImg(frontImage) }}>
-                <span className='sale-percent'>8%</span>
-                <div className="product-img text-center">
-                    <img src={img} alt="product" width={160} />
-                </div>
-                <div className="product-details">
-                    <a href='/' className='product-cat text-decoration-none'>{category}</a>
-                    <h4 className='mt-1 mb-2'>
-                        <a href="/" className='text-decoration-none'>{title}</a>
-                    </h4>
-                    <div className="product-rate">
-                        <ul className='d-flex justify-content-start align-items-center ps-0 mb-1'>
-                            <li className='list-unstyled'><Star fontSize={8} /></li>
-                            <li className='list-unstyled'><Star fontSize={8} /></li>
-                            <li className='list-unstyled'><Star fontSize={8} /></li>
-                            <li className='list-unstyled'><Star fontSize={8} /></li>
-                            <li className='list-unstyled'><Star fontSize={8} /></li>
-                            <li className='list-unstyled'>0</li>
-                        </ul>
+        <>
+            <Col sm={6} md={2}>
+                <div className='product-card h-100 py-3' onMouseEnter={() => { setImg(backImage) }} onMouseLeave={() => { setImg(frontImage) }}>
+                    <span className='sale-percent'>8%</span>
+                    <div className="product-img text-center">
+                        <img src={img} alt="product" width={160} />
                     </div>
-                    <span className='store'>By <a href="/" className='text-decoration-none'>{store}</a></span>
-                    <div className="prices-add d-flex justify-content-between align-items-center mt-2">
-                        <div className="prices d-flex align-items-center">
-                            <div className="disc-price me-2">${discount}</div>
-                            <div className="gen-price">${price}</div>
+                    <div className="product-details">
+                        <a href='/' className='product-cat text-decoration-none'>{category}</a>
+                        <h4 className='mt-1 mb-2'>
+                            <a href="/" className='text-decoration-none'>{title}</a>
+                        </h4>
+                        <div className="product-rate">
+                            <ul className='d-flex justify-content-start align-items-center ps-0 mb-1'>
+                                <li className='list-unstyled'><Star fontSize={8} /></li>
+                                <li className='list-unstyled'><Star fontSize={8} /></li>
+                                <li className='list-unstyled'><Star fontSize={8} /></li>
+                                <li className='list-unstyled'><Star fontSize={8} /></li>
+                                <li className='list-unstyled'><Star fontSize={8} /></li>
+                                <li className='list-unstyled'>0</li>
+                            </ul>
                         </div>
-                        <Button variant='none' className="add-cart d-flex align-items-center" onClick={()=>{addItem(allData)}}>
-                            <Cart className='me-1' /> Add
-                        </Button>
+                        <span className='store'>By <a href="/" className='text-decoration-none'>{store}</a></span>
+                        <div className="prices-add d-flex justify-content-between align-items-center mt-2">
+                            <div className="prices d-flex align-items-center">
+                                <div className="disc-price me-2">${discount}</div>
+                                <div className="gen-price">${price}</div>
+                            </div>
+                            <Button variant='none' className="add-cart d-flex align-items-center" onClick={() => { succesNotify(addItem(allData)) }}>
+                                <Cart className='me-1' /> Add
+                            </Button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Col>
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={1000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                />
+            </Col>
+        </>
     )
 }
 
